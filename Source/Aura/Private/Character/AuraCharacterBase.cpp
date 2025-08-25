20f4c71fa65f89f71e7965ea74af7a8d855f9538
Javier Meneses <javier.mbarrientos@gmail.com>
2025-08-25 19:49:06 -0400

    6. Player and Enemy Characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,15 @@
+// Copyright Druid Mechanics
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
